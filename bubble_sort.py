9f8d06715f9c477d4ca58cf5ca2e8d70bf0486be
msbridges1sr <36554343+msbridges1sr@users.noreply.github.com>
2024-06-15 17:12:01 -0700

    bubble sort numbers in a list

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,34 +1,24 @@
-""" Makes a temp deepcopy of the original list sorts the temp list lowest to highest
-deepcopies the temp_list back to the original list and deletes the temp_list.
-The sort is done with an outer for loop and an inner for loop. The outer for loop
-iterates the number of times there are items, calling the inner for loop.
-The inner for loop takes each item in the list and compares it the item ahead of 
-it in the list. If it is smaller it changes place and continues the loop.
-This produces a list that has left-to-righ high-to-low values, so at the end
-the list is reversed."""
+def bubble_sort(numbers):
+    """
+    Sorts a list of numeric elements from highest to lowest.
 
-import copy
+    Args:
+        numbers (list): List of numeric elements to be sorted.
 
-list = [4 , 1, 3, 9, 7]
-temp_list = copy.deepcopy(list)
-print('Deepcopy of list, temp_list: ', temp_list)
+    Returns:
+        list: The same list sorted in descending order.
+    """
+    n = len(numbers)
+    for i in range(n - 1):
+        for j in range(n - 1):
+            if numbers[j] < numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+    return numbers
 
-for item in range(0, len(temp_list), 1):
-    print(f'\nPass number = {item}\n')
-    for index in range(0, len(temp_list)-1, 1):
-        print(f"Index: {index}, temp_list element: {temp_list[index]}")
-        curr_element = temp_list[index]
-        next_element = temp_list[index+1]
-        print(f'curr_element = {curr_element}, next_element = {next_element}')
-        if curr_element < next_element:
-            print('curr_element is less than next_element, swapping..')
-            temp_list[index+1] = curr_element
-            temp_list[index] = next_element
-        print('list_in_progress = ', temp_list)
-temp_list.reverse()
-print('Finished, sorted temp_list low-high = ', temp_list)
+def main():
+    test_list = [4, 1, 3, 9, 7]
+    print(f'Unsorted list:         {test_list}')
+    print(f'Sorted list decending: {bubble_sort(test_list)}')
 
-print('Deep copying temp_list to list, and deleting temp_list..')
-list = copy.deepcopy(temp_list)
-del(temp_list)
-print('Final sorted list = ', list)
+if __name__ == '__main__':
+    main()
